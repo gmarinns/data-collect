3aeaf9a419b3341cedc41af9a1195eb446059548
Guilherme Marins <gmarins@id.uff.br>
2024-11-23 22:10:00 -0300

    Jovem Nerd

--- TabNews/basic_content.py
+++ TabNews/basic_content.py
@@ -1,15 +1,53 @@
 #%%
+import time
 import requests
+import json
+import pandas as pd
+import datetime
+ 
+
+# %%
+def get_response(**kwargs):
+    url = "https://www.tabnews.com.br/api/v1/contents"
+    resp = requests.get(url)
+    return resp
 
 # %%
 
-url = "https://www.tabnews.com.br/api/v1/contents"
+def save_data(data,option='json'):
+    now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S.%f")
 
-resp = requests.get(url)
+    if option == 'json':
+        with open(f"/Data - Collect/TabNews/data/contents/json/{now}.json", 'w') as open_file:
+            json.dump(data, open_file, indent=4)
+
+    elif option == 'parquet':
+        df = pd.DataFrame(data)
+        df.to_parquet(f"/Data - Collect/TabNews/data/contents/json/{now}.parquet", index=False)
 
 # %%
 
-resp.text
-# %%
+page = 1
+date_stop = pd.to_datetime('2024-03-01').date()
+while True:
+    print(page)
+    
+    resp = get_response(page=page, per_page=100, strategy="new")
+    if resp.status_code == 200:
+        data = resp.json()
+        save_data(data)
 
+        date = pd.to_datetime(data[-1]["updated_at"]).date()
+        if len(data) < 100 or date < date_stop:
+            break
+        
+        page += 1
+        time.sleep(5)
+    
+    else:
+        print(resp.status_code)
+        print(resp.json())
+        time.sleep(60 * 15)
+
+# %%        
 
